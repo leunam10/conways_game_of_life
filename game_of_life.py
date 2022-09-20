36370ff243e9f060199f91694144d3c84bb2b527
manuel <manuel.colavincenzo.ext@leonardo.com>
2022-09-20 18:43:20 +0200

    update version: workinggit add .

--- game_of_life.py
+++ game_of_life.py
@@ -13,20 +13,54 @@ class GameOfLife:
         # initialize figure for grid animation #
         self.figure, self.axes = plt.subplots()
         
+        
         # grid dimensions #
         self.nx_universe = nx_universe
         self.ny_universe = ny_universe
         self.nx = nx
         self.ny = ny
 
-        # universe initialization: nx x ny matrix #
+        # universe initialization: nx_universe x ny_universe matrix #
         self.universe = np.zeros((nx_universe, ny_universe))
         
-        # first generation: random matrix with dimnension nx < nx_universe; ny < ny_universe #
-        self.first_generation = np.random.randint(0, 2, size=(nx,ny))
+        # initial_automa: random matrix with dimnension nx < nx_universe; ny < ny_universe #
+        self.initial_automa = np.random.randint(0, 2, size=(nx,ny))
 
-        self.im = plt.imshow(self.first_generation, cmap="gray_r", interpolation="none", vmin=0, vmax=1)
+        self.first_generation = self.matrix_padding(self.universe, self.initial_automa, int(self.nx_universe/2), int(self.ny_universe/2))
+        self.im = plt.imshow(self.first_generation, cmap="bwr", interpolation="none", vmin=0, vmax=1)
 
+    def matrix_padding(self, A, B, r, c):
+
+        """
+        ----------
+        Parameters
+        ----------
+
+        A : np.array
+        larger matrix 
+
+        B: np.array
+        smaller matrix to insert in the larger one
+
+        r: int
+        first index of the position of the left-hand corner of B in A
+
+        c: int
+        second index of the position of the left-hand corner of B in A
+T        
+        -------
+        Returns
+        -------
+
+        np.array
+        the matrix A "merged" with matrix B
+
+        """
+        
+        A[r:r+B.shape[0], c:c+B.shape[1]] += B
+
+        return A
+        
     def first_generation_init(self):
         
         self.im.set_data(self.first_generation)
@@ -36,26 +70,26 @@ class GameOfLife:
     def generation_evolution(self, generation):
 
         evolution = self.im.get_array()
-
         # Previous evolution #
         evolution_prev = copy.copy(evolution)
-        
-        for i in range(self.nx):
-            for j in range(self.ny):
 
+        for i in range(self.nx_universe):
+            for j in range(self.ny_universe):
+                
                 cell_coord = (i, j)
                 cell_status = self.cell_evolution(i, j, evolution_prev)
-
+        
                 evolution[cell_coord] = cell_status
                 
         print("Generation: "+str(generation+1))
-        print(evolution)
+#        print(evolution)
 
-        print("")
- 
-#        print(evolution, generation)
-        self.im.set_array(evolution)
+        if(np.sum(evolution) == 0):
+            print("Life ended at generation "+str(generation+1)+"\n")
+            quit()
         
+        self.im.set_array(evolution)
+
         return [self.im]
          
     def cell_evolution(self, i, j, evolution):
@@ -108,11 +142,12 @@ class GameOfLife:
             # In all the other cases the dead cell remain dead #
             else:
                 cell_status = 0
-            
-        print(cell_coord, alive_cells, cell_status)
+
+
+#        print(cell_coord, alive_cells, cell_status)
         return cell_status
 
-    def eight_connected_neighbours(self, x, y ):
+    def eight_connected_neighbours(self, x, y):
         """The x- and y- components for a single cell in an eight connected grid
         
         Parameters
@@ -137,7 +172,7 @@ class GameOfLife:
                 newy = y+dy
                 if(dx == 0 and dy == 0):
                     continue
-                if(newx>=0 and newx<self.nx and newy >=0 and newy<self.ny):
+                if(newx>=0 and newx<self.nx_universe and newy >=0 and newy<self.ny_universe):
                     results.append((newx, newy))
                 
 
@@ -147,19 +182,26 @@ class GameOfLife:
     
     def make_animation(self):
 
-        ani = animation.FuncAnimation(self.figure, self.generation_evolution, init_func=self.first_generation_init, frames=10, interval=400, blit=True, repeat=False)
+        ani = animation.FuncAnimation(self.figure, self.generation_evolution, init_func=self.first_generation_init, frames=200, interval=50, blit=True, repeat=False)
+
+
+        #plt.title("Generation: "+str(generation))
+        plt.axis('off')
+
+        manager = plt.get_current_fig_manager()
+        manager.full_screen_toggle()
         plt.show()
-       
+        
 if __name__ == "__main__":
 
 
-    nx_universe = 100
-    ny_universe = 100
-    nx = 4
-    ny = 4
+    nx_universe = 50
+    ny_universe = 50
+    nx = 20
+    ny = 20
 
     
-    GameOfLife = GameOfLife(nx, ny)
+    GameOfLife = GameOfLife(nx_universe, ny_universe, nx, ny)
     GameOfLife.make_animation()
 
 
